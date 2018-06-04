27b53cd3872ddf1e8f147c4f4aa32ee18759cf42
Alon Bar David <me@alonbd.com>
2018-06-04 18:19:08 +0300

    Update symlink-code.js
    fixed bad link

--- scripts/symlink-code.js
+++ scripts/symlink-code.js
@@ -2,8 +2,8 @@ const OS = require("os");
 const {execSync} = require("child_process");
 
 if (OS.platform() === "win32"){
-	execSync("mklink /D ..\\ios ..\\lib\\ios",{cwd:__dirname});
-	execSync("mklink /D ..\\android ..\\lib\\android",{cwd:__dirname});	
+	execSync("mklink /D ..\\ios lib\\ios",{cwd:__dirname});
+	execSync("mklink /D ..\\android lib\\android",{cwd:__dirname});	
 } else {
 	execSync("ln -sf ../lib/{ios,android} ..",{cwd:__dirname});
 }
